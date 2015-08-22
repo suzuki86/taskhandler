08f5e222b46c76cfa3b2d4a6c517bc672515937f
suzuki86 <tsnr0001@gmail.com>
2015-08-22 19:08:41 +0900

    Make Setup.invoke normal class

--- lib/taskhandler.rb
+++ lib/taskhandler.rb
@@ -7,68 +7,64 @@ require 'taskhandler/option_parser'
 
 module TaskHandler
   class Runner
-
-    class << self
-
-      def argv
-        ARGV
-      end
-
-      def invoke
-        options = TaskHandler::OptionParser.parse_options(argv)
-        projects = TaskHandler::Project.new
-
-        # Parse subcommand
-        subcommand = argv[0]
-
-        if subcommand == "init"
-
-          projects.init
-          return
-
-        end
-
-        projects.load_projects(options[:file_path])
-
-
-        if subcommand == "add"
-
-          tasks_to_add = projects.build_task(argv)
-          projects.add_task(tasks_to_add)
-          projects.write_to_yaml(projects.projects)
-
-        elsif subcommand == "del"
-
-          projects.delete_task(argv[1])
-          projects.write_to_yaml(projects.projects)
-
-        elsif subcommand == "open"
-
-          projects.open_task(argv[1])
-          projects.write_to_yaml(projects.projects)
-
-        elsif subcommand == "close"
-
-          projects.close_task(argv[1])
-          projects.write_to_yaml(projects.projects)
-
-        elsif subcommand == "stats"
-
-          projects.display_stats
-
-        else
-
-          projects.display_tasks(
-            {
-              project: options[:project],
-              duedate: options[:duedate],
-              display_all: options[:display_all],
-            }
-          )
-
-        end
-      end
-
+    def argv
+      ARGV
     end
+
+    def invoke
+      options = TaskHandler::OptionParser.parse_options(argv)
+      projects = TaskHandler::Project.new
+
+      # Parse subcommand
+      subcommand = argv[0]
+
+      if subcommand == "init"
+
+        projects.init
+        return
+
+      end
+
+      projects.load_projects(options[:file_path])
+
+
+      if subcommand == "add"
+
+        tasks_to_add = projects.build_task(argv)
+        projects.add_task(tasks_to_add)
+        projects.write_to_yaml(projects.projects)
+
+      elsif subcommand == "del"
+
+        projects.delete_task(argv[1])
+        projects.write_to_yaml(projects.projects)
+
+      elsif subcommand == "open"
+
+        projects.open_task(argv[1])
+        projects.write_to_yaml(projects.projects)
+
+      elsif subcommand == "close"
+
+        projects.close_task(argv[1])
+        projects.write_to_yaml(projects.projects)
+
+      elsif subcommand == "stats"
+
+        projects.display_stats
+
+      else
+
+        projects.display_tasks(
+          {
+            project: options[:project],
+            duedate: options[:duedate],
+            display_all: options[:display_all],
+          }
+        )
+
+      end
+    end
+
   end
 end
